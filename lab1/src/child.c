3a1af0a114fdfa6848dcf625a2c3bf768361b48e
Lioueq <dtaraskaev@gmail.com>
2024-11-01 03:30:48 +0300

    add lab 1

---
+++ lab1/src/child.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+    char read_msg[100];
+    char response_msg[100];
+    // Читаем имя файла из stdin
+    read(STDIN_FILENO, read_msg, sizeof(read_msg));
+    FILE *fp = fopen(read_msg, "w");
+    if (!fp) {
+        perror("file error");
+        return -1;
+    }
+    while (1) {
+        int bytes_read = read(STDIN_FILENO, read_msg, sizeof(read_msg));
+        if (bytes_read <= 0) {
+            break;
+        }
+        int len = strlen(read_msg);
+        if (len > 0 && (read_msg[len - 2] == ';' || read_msg[len - 2] == '.')) {
+            fputs(read_msg, fp);
+            strcpy(response_msg, "Успешно");
+        } else {
+            strcpy(response_msg, "Предложение не оканчивается на . или ;");
+        }
+        // Пишем данные в stdout
+        write(STDOUT_FILENO, response_msg, strlen(response_msg) + 1);
+    }
+    fclose(fp);
+    return 0;
+}
---
+++ lab1/src/parent.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    int pipe1[2];
+    int pipe2[2];
+    pid_t pid;
+    if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
+        perror("pipe failed");
+        return 1;
+    }
+    pid = fork();
+    if (pid == -1) {
+        perror("fork failed");
+        return 1;
+    }
+    // pipe[0] - чтение, pipe[1] - запись
+    if (pid == 0) {
+        close(pipe1[1]);
+        close(pipe2[0]);
+
+        dup2(pipe1[0], STDIN_FILENO); // Перенаправляем стандартный ввод на pipe1[0]
+        dup2(pipe2[1], STDOUT_FILENO); // Перенаправляем стандартный вывод на pipe2[1]
+
+        close(pipe1[0]);
+        close(pipe2[1]);
+
+        execlp("./child", "child", NULL);
+        perror("execlp failed");
+        exit(1);
+    } 
+    else {
+        char msg[100];
+        char read_msg[100];
+        close(pipe1[0]);
+        close(pipe2[1]);
+        printf("write a doc name\n");
+        fgets(msg, sizeof(msg), stdin);
+        write(pipe1[1], msg, strlen(msg) + 1);
+        while (1) {
+            printf("Enter a message: ");
+            fgets(msg, sizeof(msg), stdin);
+            if (strncmp(msg, "exit", 4) == 0) {
+                break;
+            }
+            write(pipe1[1], msg, strlen(msg) + 1);
+            read(pipe2[0], read_msg, sizeof(read_msg));
+            printf("Parent read: %s\n", read_msg);
+        }
+        close(pipe1[1]);
+        close(pipe2[0]);
+        waitpid(pid, NULL, 0);
+    }
+    return 0;
+}
