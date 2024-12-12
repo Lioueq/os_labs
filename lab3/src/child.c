5ba8183705e384b3197fe00538d59ed0870d4ba7
Lioueq <dtaraskaev@yandex.ru>
2024-12-13 00:15:54 +0300

    s)DD

---
+++ lab3/src/child.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+#define SHARED_MEMORY_SIZE 256
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <shared_memory_name>\n", argv[0]);
+        return 1;
+    }
+
+    const char *shared_memory_name = argv[1];
+    int fd = shm_open(shared_memory_name, O_RDWR, 0666);
+    if (fd == -1) {
+        perror("shm_open");
+        return 1;
+    }
+
+    char *shared_memory = mmap(NULL, SHARED_MEMORY_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (shared_memory == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
+
+    char *read_msg = shared_memory;
+    char *response_msg = shared_memory + 128;
+
+    while (strlen(read_msg) == 0) {
+        sleep(1); // Ждем, пока родительский процесс запишет имя файла
+    }
+
+    FILE *fp = fopen(read_msg, "w");
+    if (!fp) {
+        perror("file error");
+        return -1;
+    }
+
+    while (1) {
+        sleep(1);
+        if (strncmp(response_msg, "exit", 4) == 0) {
+            break;
+        }
+        int len = strlen(read_msg);
+        if (len > 0 && (read_msg[len - 2] == ';' || read_msg[len - 2] == '.')) {
+            fputs(read_msg, fp);
+            strcpy(response_msg, "Success");
+        } else {
+            strcpy(response_msg, "Not over in ';' or '.'");
+        }
+    }
+    fclose(fp);
+
+    return 0;
+}
---
+++ lab3/src/parent.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+#define SHARED_MEMORY_SIZE 256
+
+int main() {
+    const char *shared_memory_name = "/shared_memory";
+    int fd = shm_open(shared_memory_name, O_CREAT | O_RDWR, 0666);
+    ftruncate(fd, SHARED_MEMORY_SIZE);
+
+    char *shared_memory = mmap(NULL, SHARED_MEMORY_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (shared_memory == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        return 1;
+    }
+
+    if (pid == 0) {
+        // Дочерний процесс
+        execlp("./child", "./child", shared_memory_name, NULL);
+        perror("execlp");
+        return 1;
+    } else {
+        // Родительский процесс
+        printf("Write a doc name\n");
+        fgets(shared_memory, SHARED_MEMORY_SIZE, stdin);
+        // Удаляем символ новой строки
+        shared_memory[strcspn(shared_memory, "\n")] = 0;
+
+        while (1) {
+            printf("Enter a message (exit for exit)\n");
+            fgets(shared_memory, SHARED_MEMORY_SIZE, stdin);
+            if (strncmp(shared_memory, "exit", 4) == 0) {
+                strcpy(shared_memory + 128, "exit");
+                break;
+            }
+            // Ожидаем ответ от дочернего процесса
+            usleep(100000);
+            printf("Status: %s\n", shared_memory + 128);
+        }
+
+        waitpid(pid, NULL, 0);
+        shm_unlink(shared_memory_name);
+    }
+
+    return 0;
+}
